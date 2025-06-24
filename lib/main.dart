862faa6b3d6a7de4646b0b1450ba130caf9c5095
claudeacahaya <claudeacc06@gmail.com>
2025-06-24 15:53:13 +0700

    membuat agar auto login

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 import 'login.dart';
+import 'dashboard.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -9,18 +10,23 @@ Future<void> main() async {
     anonKey:
         'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZraWF6amltYWF3cGRuamFmeXR4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDk5NTgxOTAsImV4cCI6MjA2NTUzNDE5MH0.H0Ixd0XxgxaZdNTURDbDicc_q0QgrZmeRpGzD6jp0kA',
   );
-  runApp(const MyApp());
+
+  final session = Supabase.instance.client.auth.currentSession;
+
+  runApp(MyApp(isLoggedIn: session != null));
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  final bool isLoggedIn;
+  const MyApp({super.key, required this.isLoggedIn});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Makan Apa Hari Ini',
       theme: ThemeData(primarySwatch: Colors.brown),
       debugShowCheckedModeBanner: false,
-      home: const LoginPage(),
+      home: isLoggedIn ? const DashboardPage() : const LoginPage(),
     );
   }
 }
